cfca769c55ed48793da004481ce1688cb6da1afe
Sami Leppänen <sami.leppanen@edu.keuda.fi>
2025-09-09 10:48:56 +0300

    lumoakatemia

---
+++ harjoitus.js
@@ -0,0 +1,40 @@
+
+console.log("Hei maailma");
+console.log("Pieni possu sanoi," + "\"Olen iloinen\"");
+
+var l1 = 3;
+var l2 = 4;
+console.log(l1+l2)
+
+console.log(l1-l2)
+
+console.log(l1*l2)
+
+console.log(l1/l2)
+
+var celcius =(100);
+var fahrenheit =(celcius * 1.8+32)
+console.log(`${fahrenheit} Fahrenheitastetta on ${celcius} Celciusastetta`);
+
+fahrenheit =(100);
+celcius=(fahrenheit - 32)/1.8;
+console.log(`${celcius} Celciusastetta on ${fahrenheit} Fahrenheitastetta`);
+
+var ml =(10);
+var km =(ml * 1.60934);
+console.log(`${km} Kilometriä on ${ml} Mailia`);
+
+km =(10);
+ml =(km / 1.60934);
+console.log(`${ml} Mailia on ${km} kilometriä`);
+
+var tuuma =(10);
+var sentti =(tuuma * 2.54);
+console.log(`${tuuma} Tuumaa on ${sentti} senttiä`);
+
+sentti =(30);
+tuuma =(sentti / 2.54);
+console.log(`${sentti} Senttiä on ${tuuma} tuumaa`);
+
+
+
