999f2b8857461be829f0c14d970f20a8a4954eff
Sami Leppänen <sami.leppanen@edu.keuda.fi>
2025-09-16 13:50:28 +0300

    java

--- elseif.js
+++ elseif.js
@@ -12,7 +12,7 @@ else {
 suurempi(3, 2);
 suurempi(1,4);
 
-//ei2
+//ei2 Tee ohjelma, joka etsii kolmesta luvusta suurimman
 
 function suurempi (a, b, c) {
 if(a > b && a > c)
@@ -32,7 +32,7 @@ suurempi(3,2,1);
 suurempi(2,1,3);
 suurempi(2,2,2);
 
-//ei4
+//ei4 Tee ohjelma, joka kysyy käyttäjältä lukua ja tarkistaa, onko se 5:llä tai 11:sta jaollinen
 
 function jaollinen(luku) {
 if (luku % 5 == 0 && luku % 11 == 0) {
@@ -51,7 +51,8 @@ jaollinen(50);
 jaollinen(44);
 jaollinen(56);
 
-//ei5
+//ei5 Tee ohjelma, joka tarkastaa, onko annettu luku karkausvuosi vai ei
+
 function karkaus(vuosi) {
     if (vuosi % 4 == 0 && vuosi % 100 != 0)
     console.log(`vuosi on karkausvuosi`);
@@ -66,3 +67,50 @@ karkaus(2000)
 karkaus(2050)
 karkaus(2014)
 karkaus(2026)
+
+//välissä 6-10
+
+//ei11 Tee ohjelma, joka laskee annetuista myynti ja ostoluvuista, saitko voittoa vai tappiota
+
+function voittoTappio(myynti, osto) {
+myynti = parseInt(myynti);
+osto = parseInt(osto);
+if ((myynti - osto) > 0) {
+console.log("Sait voittoa: " + (myynti - osto));
+}
+else {
+console.log("Tappiosi oli " + (myynti - osto));
+}
+}
+voittoTappio(30, 20);
+voittoTappio(20, 30);
+voittoTappio(30, 30);
+
+//ei12 Tee ohjelma, joka pyytää viiden kokeen pisteitä (Ma, KeFy, Äi, Ru, En -> 0 - 100) ja antaa arvion seuraavasti: Yli 90 pistettä = K5, yli 80 pistettä = H4, yli 70 pistettä = H3, yli 60
+
+function koePisteet(Ma, KeFy, Ai, Ru, En) {
+var taulukko = [["Matematiikka", Ma], ["KemiaFysiikka", KeFy], ["Aidinkieli", Ai], ["Ruotsi", Ru], ["Englanti", En]];
+for (var i = 0; i < taulukko.length; i++) {
+if (taulukko[i][1] >= 90) {
+console.log(taulukko[i][0] + ": K5");
+}
+else if (taulukko[i][1] >= 80) {
+console.log(taulukko[i][0] + ": H4");
+}
+else if (taulukko[i][1] >= 70) {
+console.log(taulukko[i][0] + ": H3");
+}
+else if (taulukko[i][1] >= 60) {
+console.log(taulukko[i][0] + ": T2");
+}
+else if (taulukko[i][1] >= 50) {
+console.log(taulukko[i][0] + ": T1");
+}
+else {
+console.log(taulukko[i][0] + ": Hylätty");
+}
+}
+}
+koePisteet(55, 78, 63, 88, 94);
+koePisteet(99, 45, 70, 80, 60);
+    
