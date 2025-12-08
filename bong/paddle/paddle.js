1538d58c42d9e7895ae8b29b4334c32cf00c4a06
unknown <sami.leppanen@edu.keuda.fi>
2025-12-08 13:50:24 +0200

    bong

--- bong/paddle/paddle.js
+++ bong/paddle/paddle.js
@@ -4,9 +4,18 @@ class paddle {
         this.y = height / 2;
         this.height = 80;
         this.width = 20; 
+
+        this.isUp = false;
+        this.isDown = false;
     }
     display() {
         fill(255);
         rect(this.x, this.y, this.width, this.height);
+    }
+    up() {
+        this.y -= 2;
+    }
+    down() {
+        this.y += 2;
     }
 }
--- bong/paddle/sketch.js
+++ bong/paddle/sketch.js
@@ -7,13 +7,17 @@ function setup() {
   }
 
 let playerPaddle;
+let aiPaddle;
 
 function setup() {
     createCanvas(624,351);
     playerPaddle = new paddle(26);   
+    aiPaddle = new paddle(width -48)
 }
 
 function draw() {
     background(0);
     playerPaddle.display();
+    aiPaddle.display();
+    
 }
